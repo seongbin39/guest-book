7f67bd4a8f42f593ecaeb084b13062dad5e1d79f
seongbin3931 <seongbin3931@gmail.com>
2021-10-27 11:27:08 +0900

    guest-book ver 1.0

---
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -0,0 +1,42 @@
+package org.zerock.guestbook.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.guestbook.entity.Guestbook;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestbookRepositoryTests {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies() {
+
+        IntStream.rangeClosed(1,300).forEach(i->{
+
+            Guestbook guestbook = Guestbook.builder()
+                    .title("title...."+i)
+                    .content("Content..."+i)
+                    .writer("user"+(i%10))
+                    .build();
+            guestbookRepository.save(guestbook);
+        });
+    }
+
+    @Test
+    public void updateTest() {
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+        if( result.isPresent()) {
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed Title....");
+            guestbook.changeContents("Changed Content...");
+
+            guestbookRepository.save(guestbook);
+        }
+    }
+}
